bc1b51ab5f7cdf62755fb01e67e698e34369fcfd
kowais915 <kowais915@gmail.com>
2023-05-27 17:54:31 +0500

    :white_check_mark: Done with the api router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 
+
+
+// get all
 router.get('/', (req, res) => {
-    res.json({msg: " This is the first route."})
+    res.json({msg: " This is all you have in your database"})
 })
 
+
+// post
+router.post('/', (req, res) => {
+    res.json({msg: " You posted a resource to the database."})
+})
+
+// get a single resource
+router.post("/:id", (req, res) => {
+    res.json({msg: "You got a single resource"})
+})
+
+// update a resource
+router.patch("/:id", (req, res) => {
+    res.json({msg: "You updated a resource"})
+})
+
+// delete a resource
+router.delete("/:id", (req, res) => {
+    res.json({msg: "You deleted a resource"})
+})
 
 module.exports = router;
